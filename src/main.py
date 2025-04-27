846a83af3069b71e88ebdf089c3aeda40e10b8f2
Tenco <theancientjammer@gmail.com>
2025-04-27 14:00:16 +0000

    Intial setup complete

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    textnode = TextNode('This is some text', 'link', 'https://www.boot.dev')
+    print(textnode)
+    
+
+if __name__ == "__main__":
+    main()
