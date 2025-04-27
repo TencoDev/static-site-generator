28dd7e78e3c6c77810d26de9a7e09420288d3a91
Tenco <theancientjammer@gmail.com>
2025-04-27 15:18:56 +0000

    Add HTMLNode and UnitTests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        html_node = HTMLNode("p", "welcome to our website", None, {"href": "https://www.google.com", "target": "_blank",})
+        props_string = html_node.props_to_html()
+        ideal_string =  ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(props_string, ideal_string)
+        
+    def test_values(self):
+        node = HTMLNode("div", "Container")
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "Container")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+        
+        
+    def test_repr(self):
+        node = HTMLNode("p","What a strange world",None,{"class": "primary"})
+        self.assertEqual(node.__repr__(), "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
