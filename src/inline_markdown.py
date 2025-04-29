da1bdb4c51eadd70c78fcc1e852701ac81ae92d2
Tenco <theancientjammer@gmail.com>
2025-04-29 13:34:18 +0000

    blocks added

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -108,6 +108,15 @@ def text_to_textnodes(text):
 
     return nodes
     
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
 
     
                  
