0375f4d30233a0ce7a10dd73e2c99704776a6034
Tenco <theancientjammer@gmail.com>
2025-04-29 12:37:37 +0000

    Add md to textnodes and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -95,4 +95,19 @@ def split_nodes_links(old_nodes):
         if text:
             new_nodes.append(TextNode(text, TextType.TEXT))
     
-    return new_nodes              
+    return new_nodes 
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_links(nodes)
+
+    return nodes
+    
+
+    
+                 
